7b4b29294de4534fc945d8acdff343562d7181a9
anagabrielaguerrero <156696695+anagabrielaguerrero@users.noreply.github.com>
2024-02-16 16:43:32 -0600

    Update VIP_Funnel.py
    se añadieron tabs

--- VIP_Funnel.py
+++ VIP_Funnel.py
@@ -11,6 +11,8 @@ st.set_page_config(layout="wide")
 st.title('VIP Funnel')
 
 
+
+
 option = st.selectbox(
     'Mes',
     ('Octubre 2023','Noviembre 2023','Diciembre 2023', 'Enero 2024'))
@@ -34,33 +36,38 @@ if option == 'Enero 2024':
 nodes = list(prev.prev_action)
 nodes.append('Click on Button for purchase membership')
 nodes_post = np.unique(post.Target, return_index=True)
-nodes_post1_t = np.unique(post1.Target, return_index=True)
+nodes_post1_t = list(post1.Target)
 nodes_post1_s = np.unique(post1.Source, return_index=True)
 
-colors =mcp.gen_color(cmap="viridis",n=len(nodes)+len(nodes_post[1])+len(nodes_post1_t[1]))
-
+colors =mcp.gen_color(cmap="viridis",n=len(nodes)+len(nodes_post[1])+len(nodes_post1_t))
 #Links de prev actions
 links = []
 for i in range(len(nodes)-1):
     links.append({'source': i, 'target': len(nodes)-1, 'value': prev.num_actions[i], 'color': colors[i]})
 
-#Links de post1 actions 
-for i in range(len(nodes_post1_s[1])): 
-        for j in indices_por_valor['Indices'][nodes_post[1][i]]:
-                links.append({'source':nodes_post[1][i] +len(nodes), 
-                        'target': j+ len(nodes_post[0]) +len(nodes),
-                        'value': post1.Clicks[j],
-                        'color': colors[i+len(nodes)+len(nodes_post)]})
+#Links de post actions 
+for i in range(len(nodes_post[1])):
+    links.append({'source':len(nodes)-1, 'target': nodes_post[1][i] +len(nodes), 'value': post.Clicks[nodes_post[1][i]], 'color': colors[i]})
 
 indices_por_valor = post1.groupby('Source').apply(lambda x: x.index.tolist()).reset_index(name='Indices')
 
+index = [indices_por_valor.Source[indices_por_valor.Source == nodes_post1_s[0][x]].index for x in range(len(nodes_post1_s))]
+index = [indice[0] for indice in index]
+
+
 #Links de post1 actions 
-for i in range(len(nodes_post1_s[1])): 
-    for j in post1.Target[post1.Source == nodes_post1_s[0][i]].index:
-        links.append({'source':nodes_post[1][i] +len(nodes), 
-                'target': labels.index(nodes_post1_t[0][j]), 
-                'value': post1.Clicks[nodes_post1_t[1][j]],
-                'color': colors[i+len(nodes)+len(nodes_post)]})
+for i in nodes_post1_s[1]: 
+        a = 0
+        for j in indices_por_valor['Indices'][i]:
+                links.append({'source':nodes_post[1][i] +len(nodes), 
+                        'Source name': nodes_post[0][i],
+                        'target': j+ len(nodes_post[0]) +len(nodes),
+                        'Target name': post1.Target[j],
+                        'value': post1.Clicks[j],
+                        'color': colors[i+len(nodes)+len(nodes_post)]})
+
+
+prev.rename(columns= {'num_actions':'# Clicks','prev_action':'Acción previa'}, inplace = True)
 
 fig = go.Figure(data=[go.Sankey(
     node=dict(
@@ -78,29 +85,23 @@ fig = go.Figure(data=[go.Sankey(
 
 fig.update_layout(title_text=option, font_size=15, autosize=False, width=1500, height=1000)
 
-prev.rename(columns= {'num_actions':'# Clicks','prev_action':'Acción previa'}, inplace = True)
+tab0, tab1, tab2, tab3 = st.tabs(['Funnel Subscriptions',"Acciones previas", "Cambio de flujo", "Acciones posteriores "])
 
-st.plotly_chart(fig, use_container_width=True)
 
-col1, col2, col3 = st.columns(3)
+with tab0:
+    st.header("Funnel Subscriptions")
+    st.plotly_chart(fig, use_container_width=True)
 
-with col1:
-    st.write("Acciones previas")
+with tab1:
+    st.header("Acciones previas")
     st.table(prev)
 
-    # st.dataframe(prev,use_container_width=True)
-    # st.markdown(prev.style.hide(axis="index").to_html(), unsafe_allow_html=True)
 
-with col2:
-    st.write("Cambio de flujo")
-    # st.dataframe(post,use_container_width=True)
+with tab2:
+    st.header("Cambio de flujo")
     st.table(post)
 
-    # st.markdown(post.style.hide(axis="index").to_html(), unsafe_allow_html=True)
-
-with col3:
+with tab3:
     st.write("Acciones posteriores")
     st.table(post1)
-    # st.dataframe(post1,use_container_width=True)
-    # st.markdown(post1.style.hide(axis="index").to_html(), unsafe_allow_html=True)
 
