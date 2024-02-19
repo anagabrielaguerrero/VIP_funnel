ebf92291c9f1eb30cc7879503efd8821d88f6e71
anagabrielaguerrero <156696695+anagabrielaguerrero@users.noreply.github.com>
2024-02-19 16:48:55 -0600

    Update VIP_Funnel_cancel.py

--- pages/VIP_Funnel_cancel.py
+++ pages/VIP_Funnel_cancel.py
@@ -1,8 +1,53 @@
+import sqlalchemy as sal
+from sqlalchemy import create_engine
+from sqlalchemy import URL
 import pandas as pd 
 import streamlit as st
 import plotly.graph_objects as go
 from mycolorpy import colorlist as mcp
 import numpy as np
+
+
+url_object = URL.create(
+    "mysql",
+    username='gabriela_guerrero',
+    password='aPTLPgXPhyLH',  # plain (unescaped) text
+    host='bi.cpi0yoqzrjqz.us-west-1.rds.amazonaws.com',
+    database='matrix',
+)
+engine = create_engine(url_object)
+connection = engine.connect()
+
+#old vs new
+q = '''
+SELECT
+   count(*) as total, extract(year_month from cancel_date) as yearmo, is_first_subscription, if(cancel_reason = 'canceled while user doing upgrade to year subscription', 'Upgrade Attempt','True manual cancel') as type
+   FROM vip_ledger
+WHERE
+    cancel_date >= '2023-09-01'
+    AND (is_manual_cancel = 1 OR is_resumed_cancellation = 1)
+    AND sku_id IN (518, 1222)
+	group by extract(year_month from cancel_date), is_first_subscription,type;
+'''
+old_new = pd.read_sql(q,connection)
+
+#exitosos
+q = '''
+SELECT count(*), extract(year_month from cancel_date) as yearmo, is_first_subscription from (
+	 SELECT
+	 *, 
+	 lead(membership_status,1,'n/a') OVER (PARTITION BY user_id ORDER BY id) AS next_membership,
+	 lead(sku_id,1,'n/a') OVER (PARTITION BY user_id ORDER BY id) AS next_sku_id
+   FROM vip_ledger
+	 where membership_status in (1,4)
+	 ) as l
+WHERE
+    l.cancel_date >= '2023-09-01'
+    AND (l.is_manual_cancel = 1 OR l.is_resumed_cancellation = 1)
+    AND l.sku_id IN (518, 1222) AND l.cancel_reason  = 'canceled while user doing upgrade to year subscription'
+		and l.next_sku_id in (873,519) group by extract(year_month from cancel_date),is_first_subscription order by yearmo
+'''
+success = pd.read_sql(q,connection)
 
 
 st.set_page_config(
@@ -15,71 +60,58 @@ st.title('VIP Funnel')
 
 st.sidebar.header("Cancellations")
 
+#format 
+old = old_new[old_new['is_first_subscription']== b'\x00']   #ord(b'\x00' 
+new = old_new[old_new['is_first_subscription']== b'\x01'] 
+old_m = old.groupby('yearmo').agg({'total': 'sum'})  # todos por mes 
+new_m = new.groupby('yearmo').agg({'total': 'sum'})
+old_success = success[success['is_first_subscription']== b'\x00']  
+new_success = success[success['is_first_subscription']== b'\x01'] 
+old['Source'] = 'Old' #añadiendo nodos 
+new['Source'] = 'New'
+old_success['Source'] = 'Upgrade Attempt Old'
+new_success['Source'] = 'Upgrade Attempt New'
+old.loc[old['type'] == 'Upgrade Attempt', 'type'] = 'Upgrade Attempt Old'
+new.loc[new['type'] == 'Upgrade Attempt', 'type'] = 'Upgrade Attempt New'
+old.loc[old['type'] == 'Upgrade Attempt', 'type'] = 'Upgrade Attempt Old'
+new.loc[new['type'] == 'Upgrade Attempt', 'type'] = 'Upgrade Attempt New'
 
 
 option = st.selectbox(
     'Mes',
-    ('Octubre 2023','Noviembre 2023','Diciembre 2023', 'Enero 2024'))
-if option == 'Octubre 2023':
-    prev = pd.read_csv('Fil/logs_202310_prev.csv')
-    post = pd.read_csv('Mau/202310_post.txt',sep= '	')  
-    post1 = pd.read_csv('Mau/202310_post1.txt', sep =  '	',index_col=False) 
+    ('Septiembre 2023','Octubre 2023','Noviembre 2023','Diciembre 2023', 'Enero 2024'))
+if option == 'Septiembre 2023':
+    yearmo = 202309
+elif option == 'Octubre 2023':
+    yearmo = 202310
 elif option == 'Noviembre 2023':
-    prev = pd.read_csv('Fil/logs_202311_prev.csv')
-    post = pd.read_csv('Mau/202311_post.txt',sep= '	')  
-    post1 = pd.read_csv('Mau/202311_post1.txt', sep =  '	',index_col=False) 
+    yearmo = 202311
 if option == 'Diciembre 2023':
-    prev = pd.read_csv('Fil/logs_202312_prev.csv')
-    post = pd.read_csv('Mau/202312_post.txt',sep= '	')  
-    post1 = pd.read_csv('Mau/202312_post1.txt', sep =  '	',index_col=False) 
+    yearmo = 202312
 if option == 'Enero 2024':
-    prev = pd.read_csv('Fil/logs_202401_prev.csv')
-    post = pd.read_csv('Mau/202401_post.txt',sep= '	')  
-    post1 = pd.read_csv('Mau/202401_post1.txt', sep =  '	',index_col=False) 
+    yearmo = 202401
 
-#Format -----------
-prev['Target'] = 'Click on Button for purchase membership' 
-prev.rename(columns= {'num_actions':'Clicks','prev_action':'Source'}, inplace = True)
-carousel = prev[prev.apply(lambda row: row.astype(str).str.contains('Clicked carousel image').any(), axis=1)]
-prev2 = prev.drop(index=carousel.index)
-new_row = pd.DataFrame.from_dict({'Clicks':[carousel.Clicks.sum()], 'Source':['Clicked carousel image'],'Target':['Click on Button for purchase membership']})
-prev2 = pd.concat([prev2,new_row], ignore_index=True)
+funnel = pd.DataFrame.from_dict({'Source':['Total Manual','Total Manual'], 'Target': ['Old','New'], 'Value': [old_m.loc[yearmo, 'total'],new_m.loc[yearmo, 'total']]})
+flux_old = pd.DataFrame({'Source':list(old[old['yearmo'] == yearmo]['Source']), 'Target':list(old[old['yearmo'] == yearmo]['type']), 'Value':list(old[old['yearmo'] == yearmo]['total'])})
+flux_new = pd.DataFrame({'Source':list(new[new['yearmo'] == yearmo]['Source']), 'Target':list(new[new['yearmo'] == yearmo]['type']), 'Value':list(new[new['yearmo'] == yearmo]['total'])})
+flux_old_success = pd.DataFrame({'Source':list(old_success[old_success['yearmo'] == yearmo]['Source']), 'Target':['Successful '], 'Value':list(old_success[old_success['yearmo'] == yearmo]['count(*)'])})
+flux_new_success = pd.DataFrame({'Source':list(new_success[new_success['yearmo'] == yearmo]['Source']), 'Target':['Successful '], 'Value':list(new_success[new_success['yearmo'] == yearmo]['count(*)'])})
+flux_new_fail = pd.DataFrame({'Source':'Upgrade Attempt New', 'Target': 'Failed upgrade', 'Value':[flux_new[flux_new['Target'] == 'Upgrade Attempt New']['Value'].values[0]-new_success[new_success['yearmo'] == yearmo]['count(*)'].values[0]]}) 
+flux_old_fail = pd.DataFrame({'Source':'Upgrade Attempt Old', 'Target': 'Failed upgrade', 'Value': [flux_old[flux_old['Target'] == 'Upgrade Attempt Old']['Value'].values[0]- old_success[old_success['yearmo'] == yearmo]['count(*)'].values[0]]})
 
-#% en dataframe 
-prev.insert(1,'%',[round(x*100/prev.Clicks.sum(),2) for x in list(prev.Clicks) ])
-post.insert(1,'%',[round(x*100/post.Clicks.sum(),2) for x in list(post.Clicks) ])
-post1.insert(1,'%',[round(x*100/post1.Clicks.sum(),2) for x in list(post1.Clicks) ])
-prev2.insert(1,'%',[round(x*100/prev2.Clicks.sum(),2) for x in list(prev2.Clicks) ])
-
-#4% en otros
-otros_prev2 = prev2[prev2['%']<4]
-otros_post1 =  post1[post1['%']<4] 
-otros_int = otros_post1[otros_post1.Source == 'Interacting With Payment Page']
-otros_change = otros_post1[otros_post1.Source == 'Change Flow']
-
-new_prev2 = prev2.drop(index= otros_prev2.index)
-new_post1 =  post1.drop(index= otros_post1.index)
-new_row_prev2 = pd.DataFrame.from_dict({'Clicks':[otros_prev2.Clicks.sum()], 'Source':['Otras acciones'],'Target':['Click on Button for purchase membership']})
-new_row_post1_1 = pd.DataFrame.from_dict({'Clicks':[otros_int.Clicks.sum()], 'Source':['Interacting With Payment Page'],'Target':['Otras acciones en payment']})
-new_row_post1_2 = pd.DataFrame.from_dict({'Clicks':[otros_change.Clicks.sum()], 'Source':['Change Flow'],'Target':['Otras acciones en flujo']})
-new_prev2 = pd.concat([new_prev2,new_row_prev2], ignore_index=True)
-new_post1 = pd.concat([new_post1,new_row_post1_1,new_row_post1_2], ignore_index=True)
-
-funnel = pd.concat([new_prev2,post,new_post1], ignore_index=True)
+funnel = pd.concat([funnel,flux_old,flux_new,flux_old_success,flux_new_success,flux_old_fail,flux_new_fail], ignore_index=True)
 colors =mcp.gen_color(cmap="viridis",n=len(funnel))
 funnel['Colors'] = colors
-
 
 
 #Diagrama ---------------------------------------------------------
 unique_source_target = list(pd.unique(funnel[['Source', 'Target']].values.ravel('K')))
 mapping_dict = {k: v for v, k in enumerate(unique_source_target)}
-
-
 funnel['Source'] = funnel['Source'].map(mapping_dict)
 funnel['Target'] = funnel['Target'].map(mapping_dict)  
 links_dict = funnel.to_dict(orient='list')
 
+# Create figure
 fig = go.Figure(data=[go.Sankey(
     node=dict(
         pad=15,
@@ -90,52 +122,21 @@ fig = go.Figure(data=[go.Sankey(
     link=dict(
       source = links_dict["Source"],
       target = links_dict["Target"],
-      value = links_dict["Clicks"],
+      value = links_dict["Value"],
       color = links_dict["Colors"]
     ),
 )],)
 
 # Update layout
-fig.update_layout(title_text=option, font_size=15, autosize=False, width=1500, height=1000)
+fig.update_layout(title_text="Enero 2024", font_size=15, autosize=False, width=1500, height=1000)
 
-
-def highlight_low_values(val):
-    color = '#AF1F00' if val < 4 else None 
-    return f'background-color: {color}'
-
-def highlight_low_values1(val):
-    color = '#AF1F00' if val < 4 else None 
-    return f'background-color: {color}'
-
-def highlight_low_values2(val):
-    color = '#bd4d4d' if val < 4 else None 
-    return f'background-color: {color}'
-
-subset_index = otros_change.index.tolist()
-styled_df = post1.style.map(highlight_low_values1, subset=pd.IndexSlice[subset_index, '%'])
-subset_index = otros_int.index.tolist()
-styled_df = styled_df.map(highlight_low_values2, subset=pd.IndexSlice[subset_index, '%'])
-
-tab0, tab1, tab2, tab3 = st.tabs(['Funnel Subscriptions',"Acciones previas", "Cambio de flujo", "Acciones posteriores "])
+tab0, tab1 = st.tabs(['Funnel Cancellations',"True cancel reasons"])
 
 
 with tab0:
-    st.header("Funnel Subscriptions")
+    st.header("Funnel Cancellations")
     st.plotly_chart(fig, use_container_width=True)
 
 with tab1:
-    st.header("Acciones previas")
-    st.write(f'Nota: Los porcentajes menores al 4% se añadieron a "Otras acciones" (resaldados en rojo)')
-
-    st.table(prev.style.map(highlight_low_values,subset=['%']))
-
-with tab2:
-    
-    st.header("Cambio de flujo")
-    st.table(post.style.map(highlight_low_values,subset=['%']))
-
-with tab3:
-    st.header("Acciones posteriores")
-    st.write(f'Nota: Los porcentajes menores al 4% se añadieron a "Otras acciones en flujo" y a "Otras acciones en payment" (resaldados en rojo)')
-    st.table(styled_df)
-
+    st.header("True cancel reasons")
+    st.write(f'Nota: PENDIENTE')
