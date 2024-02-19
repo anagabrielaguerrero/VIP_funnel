4979350f0738913231b7b940d69bc1ba9b179298
anagabrielaguerrero <156696695+anagabrielaguerrero@users.noreply.github.com>
2024-02-19 10:00:45 -0600

    Update VIP_Funnel.py
    -

--- VIP_Funnel.py
+++ VIP_Funnel.py
@@ -33,56 +33,37 @@ if option == 'Enero 2024':
     post = pd.read_csv('Mau/202401_post.txt',sep= '	')  
     post1 = pd.read_csv('Mau/202401_post1.txt', sep =  '	',index_col=False) 
 
-nodes = list(prev.prev_action)
-nodes.append('Click on Button for purchase membership')
-nodes_post = np.unique(post.Target, return_index=True)
-nodes_post1_t = list(post1.Target)
-nodes_post1_s = np.unique(post1.Source, return_index=True)
+#Format
+prev['Target'] = 'Click on Button for purchase membership' 
+prev.rename(columns= {'num_actions':'Clicks','prev_action':'Source'}, inplace = True)
+funnel = pd.concat([prev,post,post1], ignore_index=True)
+colors =mcp.gen_color(cmap="viridis",n=len(funnel))
+funnel['Colors'] = colors
 
-colors =mcp.gen_color(cmap="viridis",n=len(nodes)+len(nodes_post[1])+len(nodes_post1_t))
-#Links de prev actions
-links = []
-for i in range(len(nodes)-1):
-    links.append({'source': i, 'target': len(nodes)-1, 'value': prev.num_actions[i], 'color': colors[i]})
-
-#Links de post actions 
-for i in range(len(nodes_post[1])):
-    links.append({'source':len(nodes)-1, 'target': nodes_post[1][i] +len(nodes), 'value': post.Clicks[nodes_post[1][i]], 'color': colors[i]})
-
-indices_por_valor = post1.groupby('Source').apply(lambda x: x.index.tolist()).reset_index(name='Indices')
-
-index = [indices_por_valor.Source[indices_por_valor.Source == nodes_post1_s[0][x]].index for x in range(len(nodes_post1_s))]
-index = [indice[0] for indice in index]
+unique_source_target = list(pd.unique(funnel[['Source', 'Target']].values.ravel('K')))
+mapping_dict = {k: v for v, k in enumerate(unique_source_target)}
 
 
-#Links de post1 actions 
-for i in nodes_post1_s[1]: 
-        a = 0
-        for j in indices_por_valor['Indices'][i]:
-                links.append({'source':nodes_post[1][i] +len(nodes), 
-                        'Source name': nodes_post[0][i],
-                        'target': j+ len(nodes_post[0]) +len(nodes),
-                        'Target name': post1.Target[j],
-                        'value': post1.Clicks[j],
-                        'color': colors[i+len(nodes)+len(nodes_post)]})
-
-
-prev.rename(columns= {'num_actions':'# Clicks','prev_action':'Acción previa'}, inplace = True)
+funnel['Source'] = funnel['Source'].map(mapping_dict)
+funnel['Target'] = funnel['Target'].map(mapping_dict)  
+links_dict = funnel.to_dict(orient='list')
 
 fig = go.Figure(data=[go.Sankey(
     node=dict(
         pad=15,
         thickness=20,
         line=dict(color="black", width=0.3),
-        label=nodes + list(nodes_post[0]) + list(nodes_post1_t)
+        label= unique_source_target
     ),
     link=dict(
-        source=[link['source'] for link in links],
-        target=[link['target'] for link in links],
-        value=[link['value'] for link in links], color =[link['color'] for link in links]
+      source = links_dict["Source"],
+      target = links_dict["Target"],
+      value = links_dict["Clicks"],
+      color = links_dict["Colors"]
     ),
 )],)
 
+# Update layout
 fig.update_layout(title_text=option, font_size=15, autosize=False, width=1500, height=1000)
 
 tab0, tab1, tab2, tab3 = st.tabs(['Funnel Subscriptions',"Acciones previas", "Cambio de flujo", "Acciones posteriores "])
